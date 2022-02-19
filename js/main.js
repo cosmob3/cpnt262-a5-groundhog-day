75049863330ce48ede9a46d74d33da3a10ec180a
cosmob3 <bryanlvelasco@hotmail.com>
2022-02-18 17:10:34 -0700

    been working on code. Have not commited very much

--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
-const output = document.querySelector(".output")
+const tvShows = [
+  'Lost',
+  'Stranger Things',
+  'Breaking Bad',
+  'Game of Thrones',
+  '24',
+  'The Office',
+  'Community',
+  'Parks and Rec.',
+  'Power',
+  'BoJack Horseman',
+  'The Witcher',
+  'Homeland',
+  'Disenchantment',
+  'Euphoria',
+  'Degrassi',
+  'Drake and Josh',
+  'The Walking Dead',
+  'Adventure Time',
+  'Futurama',
+  'Family Guy',
+  'South Park',
+  'Malcolm in the Middle',
+  'Simpsons',
+  'Keeping Up With the Kardashian',
+  'SpongeBob'
+];
 
-const myArray = () => {
-  const favouriteTvShows = [
-    'Lost',
-    'Stranger Things',
-    'Breaking Bad',
-    'Game of Thrones',
-    '24',
-    'The Office',
-    'Community',
-    'Parks and Rec.',
-    'Power',
-    'BoJack Horseman',
-    'The Witcher',
-    'Homeland',
-    'Disenchantment',
-    'Euphoria',
-    'Degrassi',
-    'Drake and Josh',
-    'The Walking Dead',
-    'Adventure Time',
-    'Futurama',
-    'Family Guy',
-    'South Park',
-    'Malcolm in the Middle',
-    'Simpsons',
-    'Keeping Up With the Kardashian',
-    'SpongeBob'
-  ];
+console.log(tvShows);
+
+let content = '';
+
+for (let i = 0; i <= tvShows.length - 1; i++) {
+
+  // content = content + `${content}<li><a href="">${tvShows}</a></li>`;
+  content +=`<li><a href="">${tvShows[i]}</a></li>`;
+  
+  console.log(tvShows[i]);
 }
-console.log(myArray.length);
+
+document.querySelector('.link').innerHTML = `<ul>${content}</ul>`;
